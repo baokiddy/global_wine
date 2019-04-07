7760653d68b4bc7ee0bd1f661d85b460d9f5cfcc
Brian_B <brianbarragan412@gmail.com>
2019-04-07 14:10:44 -0700

    updated Jupyter Notebook and SQLite

---
+++ config.py
@@ -0,0 +1,2 @@
+# Enter your API key
+gkey = "AIzaSyDftfuFKrfeI7POVwh-W87J3FZCn4j8--E"
--- wine/app.py
+++ wine/app.py
@@ -27,10 +27,25 @@ Base = automap_base()
 # reflect the tables
 Base.prepare(db.engine, reflect=True)
 
+# Save references to each table
+# table = Base.classes
+
 @app.route("/")
 def index():
     """Return the homepage."""
     return render_template("index.html")
+
+@app.route("/api")
+def api():
+    # Use Pandas to perform the sql query
+    stmt = db.session.query(Base.classes.data).statement
+    df = pd.read_sql_query(stmt, db.session.bind)   
+
+    # Return a list of the column names (sample names)
+    return jsonify(df)
+
+
+
 
 if __name__ == "__main__":
     app.run()
