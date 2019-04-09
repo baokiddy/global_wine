02b92104543656b7babc96583e712a2bc9419b94
Brian_B <brianbarragan412@gmail.com>
2019-04-08 21:25:01 -0700

    new app

--- wine/app.py
+++ wine/app.py
@@ -6,30 +6,34 @@ import numpy as np
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
 
 
-#################################################
+
 # Database Setup
-#################################################
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/wineDB.sqlite"
 
-db = SQLAlchemy(app)
+engine = create_engine("sqlite:///db/wineDB.sqlite")
 
 # reflect an existing database into a new model
 Base = automap_base()
 # reflect the tables
-Base.prepare(db.engine, reflect=True)
+Base.prepare(engine, reflect=True)
 
 # Save references to each table
-# table = Base.classes
+table = Base.classes
 
+session = Session(engine)
+
+print(Base.classes.keys())
+
+print(engine.table_names())
+#for some reason it doesn't like data at the end. Not sure why as that's the table name. 
+app = Flask(__name__)
 @app.route("/")
 def index():
     """Return the homepage."""
@@ -37,15 +41,28 @@ def index():
 
 @app.route("/api")
 def api():
-    # Use Pandas to perform the sql query
-    stmt = db.session.query(Base.classes.data).statement
-    df = pd.read_sql_query(stmt, db.session.bind)   
+    results = session.query(table)
+    all_wines = []
 
-    # Return a list of the column names (sample names)
-    return jsonify(df)
-
-
+    for wine in results:
+        wine_dict = {}
+        wine_dict['country']= wine.country
+        wine_dict['description']= wine.description
+        wine_dict['points']= wine.points
+        wine_dict['price']= wine.price
+        wine_dict['province']= wine.province
+        wine_dict['region']= wine.region
+        wine_dict['taster_name']= wine.taster_name
+        wine_dict['taster_twitter_handle']= wine.taster_twitter_handle        
+        wine_dict['title']= wine.title
+        wine_dict['variety']= wine.variety
+        wine_dict['winery']= wine.winery
+        wine_dict['address']= wine.address
+        wine_dict['lats']= wine.lats
+        wine_dict['lngs']= wine.lngs
+        all_wines.append(wine_dict)
+    return jsonify(all_wines)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
