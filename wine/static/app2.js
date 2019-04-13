f519d33ef82e017f2ecd436e2a0ae4b748fa2c4c
achieng <bellaokiddy@gmail.com>
2019-04-13 10:27:52 -0700

    Corrected image issue

--- wine/static/app2.js
+++ wine/static/app2.js
@@ -9,27 +9,48 @@ d3.json("/csv").then(function(data) {
             .attr("class", "item rear-window")
             .attr("data-order", i)
             .attr("src", data[i]['winePhotos'])
+            .style("width", 113 + 'px')
+            .style("height", 200 + 'px');
     }
     
     // Pulling image from file and layering them with the correct width
     
     var picPosition = 1;
-    //var pics = data.winePhotos;
-
-    // wineData.forEach(function(data) {
-            
-    // });
         
 
     $(".rear-window").each(function() {
 
         $(this).css({left: (picPosition-1) * 15});
-        
         $(this).css("background-image", `${data.winePhotos}`);
-        // $(this).css("background-image", `url()`);
         picPosition++;
 
     });
       
-        // console.log(pics);
+
+    $(".item").on("mouseover",function() {
+          
+        var current = $(this).data().style({"height": 200 + 'px'}).order;
+        var leftdif = ((current/81)*200)/current;
+        var rightdif = (200-((current/81)*200))/(80-current);
+        var wid = 15-(200-15)/81;
+        $(".item").css({"width": "15px", "-webkit-transform": "translate(0,0)"});
+        $(this).css({"width": "215px",  "-webkit-transform": "translate(-"+(current/81)*200+"px,0)"});
+        $(this).prevAll(".item").each(function() {$( this ).css({"width": wid+"px", "-webkit-transform": "translate(-"+$(this).data().order*leftdif+"px,0)"});});
+        $(this).nextAll(".item").each(function() {$( this ).css({"width": wid+"px", "-webkit-transform": "translate("+(81-$(this).data().order)*rightdif+"px,0)"});});
+        console.log(current);
+    });
+
+    // Mouseout event where image goes back into the carousel
+    $(".item").mouseout(function() {
+      $(".item").css({"width": "15px", "-webkit-transform": "translate(0,0)"})
+
+      for(var i = 0; i < 82; i++){
+        container1.attr("class", "item rear-window")
+            .attr("data-order", i)
+            .attr("src", data[i]['winePhotos'])
+            .style("width", 113 + 'px')
+            .style("height", 200 + 'px');
+    }
+
+    });
 });
