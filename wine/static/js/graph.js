e0e540233fe9939bbf76cf945c07251c14615a47
Brian_B <brianbarragan412@gmail.com>
2019-04-13 11:06:00 -0700

    new graph

---
+++ wine/static/js/graph.js
@@ -0,0 +1,72 @@
+// sample chart
+
+// var chart = c3.generate({
+//   bindto: '#chart',
+//   data: {
+//     columns: [
+//       ['data1', 30, 200, 100, 400, 150, 250],
+//       ['data2', 50, 20, 10, 40, 15, 25]
+//     ]
+//   }
+// });
+
+// sample chart
+
+var url = '/api'
+// Grab the data with d3
+d3.json(url).then(function(data) {
+
+var topWines = data.data.filter(x => x.points > 99);
+
+var arr = topWines.map(x =>x.country);
+
+var counts = {}
+for(var i =0;i < arr.length;i++){
+    counts[arr[i]] = 1 + (counts[arr[i]] || 0);
+};
+console.log(counts)
+
+// donut chart code
+
+var chart = c3.generate({
+    data: {
+        columns: [
+            ['Australia', counts.Australia],
+            ['France', counts.France],
+            ['Italy', counts.Italy],
+            ['US', counts.US],
+        ],
+        type : 'donut',
+    },
+    donut: {
+        title: "100pt Wine Wheel"
+    }
+  });
+  
+  // donut chart code
+
+})
+
+
+var chart_2 = c3.generate({
+    data: {
+        columns: [
+            ['Pinot Noir', 89.91, 52.11],
+            ['Chardonnay', 88.94, 39.37],
+            ['Red Blend', 88.51, 35.56],
+            ['Cabernet Sauvignon', 88.86, 49.01],
+            ['Bordeaux-style Red Blend', 88.71, 44.67],
+        ],
+        type: 'bar',
+        // types: {
+        //     data3: 'spline',
+        //     data4: 'line',
+        //     data6: 'area',
+        // },
+        // groups: [
+        //     ['Pinot Noir']
+        // ]
+    }
+});
+
+
