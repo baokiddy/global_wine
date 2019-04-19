c59134b4e9626c3f9ff517580eaa98c3eeedbe7e
achieng <bellaokiddy@gmail.com>
2019-04-18 20:15:33 -0700

    Updated logic file

---
+++ wine/static/app3.js
@@ -0,0 +1,104 @@
+d3.json("/csv").then(function(data) {
+    console.log(data);
+
+    // Creating img tag within container one
+    var container1 = d3.select(".one");
+
+    for(var i = 0; i < 82; i++){
+        container1.append("img")
+            .attr("class", "item rear-window")
+            .attr("data-order", i)
+            .attr("src", data[i]['winePhotos'])
+            .style("width", 113 + 'px')
+            .style("height", 200 + 'px')
+            .on("mouseover", function() {
+
+                var current = d3.select(this).attr("data-order");
+                var pic = d3.select(".item")
+                    .attr("style", "width: 15px")
+                    .attr("style", "-webkit-transform: translate(0,0)");
+                
+                console.log(current);
+                
+                pic.attr("style", "width: 215px")  
+                    .attr("style", `-webkit-transform: translate(- + ${(current/81)*200}+ px, 0)`);
+
+                console.log(d3.select(".item").attr("width"));
+
+                d3.select(".item")
+                    .attr("style", function(d, i) { 
+                        
+                        for (k = 1; k< data.length - i; k++) {
+                            
+                            var prevData = d3.select(this). data(k).order
+                                // .attr("data-order", k)
+                            
+                            return `width :${(215 - (15*(current- prevData)))}`;
+        
+                        }
+                        
+                        console.log(prevData);
+                        
+                        // for (k = data.length -1; k> i ; k--) {
+                            
+                        //     var afterData = d3.select(this)
+                        //         .attr("data-order", k)
+
+                        //     return `width: ${(215 - (15*(afterData - current)))}`;
+        
+                        // } 
+                    })
+                    .attr("style", function(d, i) { 
+                        
+                        for (k = 1; k< data.length - i; k++) {
+                            
+                            var prevData = d3.select(this).data(k).order
+                                // .attr("data-order", k)
+
+                            var leftdif = ((current/81)*200)/current;
+
+                            return `-webkit-transform: translate(- ${prevData*leftdif} , 0)`;
+        
+                        }
+                        
+
+                    })
+                    
+    })
+
+    }
+    
+    // Pulling image from file and layering them with the correct width
+    
+    var picPosition = 1;
+        
+    $(".rear-window").each(function() {
+
+        $(this).css({left: (picPosition-1) * 15});
+        $(this).css("background-image", `${data.winePhotos}`);
+        picPosition++;
+
+    });
+      
+    // $(".item").on("mouseover",function() {
+          
+    //     var current = $(this).data().order;
+
+    //     console.log(current);
+    //     var leftdif = ((current/81)*200)/current;
+    //     var rightdif = (200-((current/81)*200))/(80-current);
+    //     var wid = 15-(200-15)/81;
+
+    //     $(".item").css({"width": "15px", "-webkit-transform": "translate(0,0)"});
+    //     $(this).css({"width": "215px",  "-webkit-transform": "translate(-"+(current/81)*200+"px,0)"});
+    //     $(this).prevAll(".item").each(function() {$( this ).css({"width": wid+"px", "-webkit-transform": "translate(-"+$(this).data().order*leftdif+"px,0)"});});
+    //     $(this).nextAll(".item").each(function() {$( this ).css({"width": wid+"px", "-webkit-transform": "translate("+(81-$(this).data().order)*rightdif+"px,0)"});});
+    //     console.log(current);
+    // });
+
+    // Mouseout event where image goes back into the carousel
+    $(".item").mouseout(function() {
+      $(".item").css({"width": "15px", "-webkit-transform": "translate(0,0)"})
+
+    });
+});
--- wine/static/js/logic.js
+++ wine/static/js/logic.js
@@ -1,20 +1,25 @@
 // Creating our initial map object
 // Define arrays to hold created city and state markers
-var pinotNoirMarkers = [];
-var chardonnayMarkers = [];
-var redBlendMarkers = [];
-var cabernetSauvignonMarkers = [];
-var all_elseMarkers = [];
+// var pinotNoirMarkers = [];
+// var chardonnayMarkers = [];
+// var redBlendMarkers = [];
+// var cabernetSauvignonMarkers = [];
+// var all_elseMarkers = [];
 
 var url = '/api'
 // Grab the data with d3
 d3.json(url).then(function(data){
+
+  // var sample = data.data.slice(0,1000)
+
   console.log(data)
 
-  console.log(data.data[0].lats)
-
   // Create a new marker cluster group
-  var markers = L.markerClusterGroup();
+  var pinotNoirMarkers = L.markerClusterGroup();
+  var chardonnayMarkers = L.markerClusterGroup();
+  var redBlendMarkers = L.markerClusterGroup();
+  var cabernetSauvignonMarkers = L.markerClusterGroup();
+  var all_elseMarkers = L.markerClusterGroup();
 
 
 // Loop through data
@@ -22,30 +27,34 @@ d3.json(url).then(function(data){
 // 	L.marker([location.lats, location.lngs]).addTo(myMap);
 for (var i = 0; i < data.data.length; i++) {
 	var location = data.data[i];
-// 	console.log(location.lngs);
+	// console.log(location.lngs);
 // 	L.marker([location.lats, location.lngs]).addTo(myMap);
   // Set the marker radius for the state by passing population into the markerSize function
   
-  if (location.variety = "Pinot Noir"){
-  	pinotNoirMarkers.push(
-    markers.addLayer(L.marker([location.lats, location.lngs])))
-  } else if (location.variety = "Chardonnay"){
-  	chardonnayMarkers.push(
-    markers.addLayer(L.marker([location.lats, location.lngs])))
-  }  else if (location.variety = "Red Blend"){
-  	chardonnayMarkers.push(
-    markers.addLayer(L.marker([location.lats, location.lngs])))
-  }  else if (location.variety = "Cabernet Sauvignon"){
-  	chardonnayMarkers.push(
-    markers.addLayer(L.marker([location.lats, location.lngs])))
-  } else
-  	{
-  	chardonnayMarkers.push(
-    markers.addLayer(L.marker([location.lats, location.lngs])))
-  }  
-}
+// markers.addLayer(L.marker([location.lats, location.lngs]))
 
-myMap.addLayer(markers)
+  if (location.variety === "Pinot Noir"){
+  	pinotNoirMarkers.addLayer(
+    L.marker([location.lats, location.lngs]).bindPopup("<h5>Variety: " + location.variety + "</h5><hr><h5>Region: " + location.region + "</h5><hr><h5>Winery: " + location.winery + "</h5>"))
+  } else if (location.variety === "Chardonnay"){
+  	chardonnayMarkers.addLayer(
+      L.marker([location.lats, location.lngs]).bindPopup("<h5>Variety: " + location.variety + "</h5><hr><h5>Region: " + location.region + "</h5><hr><h5>Winery: " + location.winery + "</h5>"))
+    }  else if (location.variety === "Red Blend"){
+  	redBlendMarkers.addLayer(
+      L.marker([location.lats, location.lngs]).bindPopup("<h5>Variety: " + location.variety + "</h5><hr><h5>Region: " + location.region + "</h5><hr><h5>Winery: " + location.winery + "</h5>"))
+    }  else if (location.variety === "Cabernet Sauvignon"){
+  	cabernetSauvignonMarkers.addLayer(
+      L.marker([location.lats, location.lngs]).bindPopup("<h5>Variety: " + location.variety + "</h5><hr><h5>Region: " + location.region + "</h5><hr><h5>Winery: " + location.winery + "</h5>"))
+    } else
+  	{
+  	all_elseMarkers.addLayer(
+      L.marker([location.lats, location.lngs]).bindPopup("<h5>Variety: " + location.variety + "</h5><hr><h5>Region: " + location.region + "</h5><hr><h5>Winery: " + location.winery + "</h5>"))
+    }  
+}
+// myMap.addLayer(markers)
+console.log(pinotNoirMarkers)
+
+// myMap.addLayer(markers)
 // 
 // for(i=0; i< data.data.length; i++){
 //   // Set the data location property to a variable
@@ -62,9 +71,9 @@ myMap.addLayer(markers)
 // myMap.addLayer(markers)
 // }
 
-})
+// })
 
-console.log(pinotNoirMarkers);
+// console.log(pinotNoirMarkers);
 // Define variables for our tile layers
 var lightmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
@@ -88,11 +97,11 @@ var streetsmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.p
 });
 
 // Create two separate layer groups below. One for city markers, and one for states markers
-var pinotNoir = L.layerGroup(pinotNoirMarkers);
-var chardonnay = L.layerGroup(chardonnayMarkers);
-var redBlend = L.layerGroup(redBlendMarkers);
-var cabernetSauvignon = L.layerGroup(cabernetSauvignonMarkers);
-var all_else = L.layerGroup(all_elseMarkers);
+// var pinotNoir = L.layerGroup(pinotNoirMarkers);
+// var chardonnay = L.layerGroup(chardonnayMarkers);
+// var redBlend = L.layerGroup(redBlendMarkers);
+// var cabernetSauvignon = L.layerGroup(cabernetSauvignonMarkers);
+// var all_else = L.layerGroup(all_elseMarkers);
 
 // Only one base layer can be shown at a time
 var baseMaps = {
@@ -103,11 +112,11 @@ var baseMaps = {
 
 // Overlays that may be toggled on or off
 var overlayMaps = {
-  "Pinot Noir": pinotNoir,
-  "Chardonnay": chardonnay,
-  "Red Blend": redBlend,
-  "cabernet Sauvignon": cabernetSauvignon,
-  "Bordeaux-style Red Blend": all_else
+  "Pinot Noir": pinotNoirMarkers,
+  "Chardonnay": chardonnayMarkers,
+  "Red Blend": redBlendMarkers,
+  "cabernet Sauvignon": cabernetSauvignonMarkers,
+  "Others": all_elseMarkers
 };
 
 // We set the longitude, latitude, and the starting zoom level=========================end
@@ -115,99 +124,13 @@ var overlayMaps = {
 var myMap = L.map("map", {
   center: [38.5025, -122.2654],
   zoom: 13,  // 0 to 24
-  maxZoom: 18,
-  layers: [streetsmap, pinotNoir, chardonnay]  // , chardonnay, redBlend, cabernetSauvignon, bordeaux]
+  maxZoom: 10,
+  layers: [streetsmap, pinotNoirMarkers]  // , chardonnay, redBlend, cabernetSauvignon, bordeaux]
 });
 
-// Adding a tile layer (the background map image) to our map
-// We use the addTo method to add objects to our map
-// L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-//   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-//   maxZoom: 18,
-//   id: "mapbox.streets",
-//   accessToken: API_KEY
-// });
 
-// Assemble API query URL
-// var url = '/api'
-// // Grab the data with d3
-// d3.json(url).then(function(data){
-//   console.log(data)
-// 
-//   console.log(data.data[0].lats)
-// 
-//   // Create a new marker cluster group
-//   var markers = L.markerClusterGroup();
-// 
-// 
-// // Loop through data
-// 
-// for(i=0; i< data.data.length; i++){
-//   // Set the data location property to a variable
-//   var location = data.data[i];
-//   // Check for location property
-//   if(location){
-// 
-//   // Add a new marker to the cluster group and bind a pop-up
-//     markers.addLayer(L.marker([location.lats, location.lngs]))
-//     .bindPopup(location.title);
-//   }
-// 
-//   // Add our marker cluster layer to the map
-// myMap.addLayer(markers)
-// }
-// 
-// })
+
 L.control.layers(baseMaps, overlayMaps, {
-  collapsed: false
+  collapsed: true
 }).addTo(myMap);
-
-
-
-
-
-// // Creating map object
-// var myMap = L.map ("map", {
-//   center: [40.7, -73.95],
-//   zoom: 11
-// });
-
-// // Adding tile layer to the map
-// L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-//   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-//   maxZoom: 18,
-//   id: "mapbox.streets",
-//   accessToken: API_KEY
-// }).addTo(myMap);
-
-// // TODO:
-
-// // Assemble API query URL
-// var url = '/api'
-// // Grab the data with d3
-// d3.json(url, function(data){
-//   console.log(data)
-
-//   // Create a new marker cluster group
-//   var markers = L.markerClusterGroup();
-
-
-// // Loop through data
-
-// for(i=0; i< data.length; i++){
-//   // Set the data location property to a variable
-//   var location = data[i];
-//   // Check for location property
-//   if(location){
-
-//   // Add a new marker to the cluster group and bind a pop-up
-//     markers.addLayer(L.marker([location.lats, location.lngs]))
-//     .bindPopup(data[i].title);
-//   }
-
-//   // Add our marker cluster layer to the map
-// myMap.addLayer(markers)
-// }
-
-// })
-
+})
